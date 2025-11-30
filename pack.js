d20b6331742c9fa961ebc3e66fe88a199418be38
Ethan Carter <55220598+EthCarterDev@users.noreply.github.com>
2025-11-30 23:10:00 +0000

    feat(*): init

--- pack.js
+++ pack.js
@@ -4,7 +4,7 @@ import AdmZip from 'adm-zip';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path'; 
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url); 
 const __dirname = dirname(__filename);
 
 try {
